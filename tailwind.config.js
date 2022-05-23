8dd011ddb8ed033a5f7c87866032aaf3eb522221
Fabio Duarte <mail.fabio.duarte@gmail.com>
2022-05-23 17:59:54 -0500

    template: brand colors added to 'tailwind.config.js'

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'webSite': {
+          'lightGray': '#F1F1F1',
+          'Gray': '#C9C9C9',
+          'green': '#70877B',
+          'black': '#161616',
+          'white': '#FFFFFF',
+          'red': '#FF0000',
+        },
+      },
       fontFamily: {
         josefin: ['Josefin Sans', 'sans-serif']
       },
